abf3d073b69952050a163782206df13077f6dbfa
Declan-E <declan_english@hotmail.com>
2021-09-20 13:23:47 +1000

    Added import functions to import csv data into local objects

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const csvFiles = []; //A list of parsed CSV files
-
-//Generates a list of all CSV files in input folder
-function getCsvFiles() {
-	var fr = new FileReader();
-}
+//Global lists of imported objects
+//In a larger system these would be encapsulated
+var barcodes = [];
+var catalogs = [];
+var suppliers = [];
 
 //Import CSVs from user selection
 //fileList - HTML input element, contains files
 function importFiles(fileList) {
 	for (var i = 0; i < fileList.files.length; i++) {
-		console.log(fileList.files[i].name); //Test
+		if (fileList.files[i].name.split('.')[1] != 'csv') break; //Exit if file does not have csv extension
+		
 		parseInputFile(fileList.files[i]);
 	}
 }
 
+//Parse given csv file and add each line as an object to the relevant object list
+//file - A user selected CSV file
 function parseInputFile(file) {
 	//Read each row and create a row object
 	let reader = new FileReader();
@@ -21,10 +23,64 @@ function parseInputFile(file) {
 	
 	//When reader is ready
 	reader.onload = function() {
-		console.log(reader.result); //Test
-		//TODO: Format results into appropriate object and add to csvFiles list
+		//const car = {type:"Fiat", model:"500", color:"white"};
+		var lines = reader.result.split('\r\n');
+		// First line is always headers
+		switch(lines[0]) {
+			case "SupplierID,SKU,Barcode":
+				// Barcode
+				importBarcodes(lines);
+				break;
+			case "SKU,Description":
+				// Catalog
+				importCatalogs(lines);
+				break;
+			case "ID,Name":
+				// Supplier
+				importSuppliers(lines);
+				break;
+			default:
+				// code block
+}
+		
 	};
     reader.onerror = function() {
 		console.log(reader.error); //Log error code to console
 	};
+}
+
+//import given lines into barcodes objects
+//fileLines - Array of lines imported for file (string array)
+function importBarcodes(fileLines) {
+	//SupplierID,SKU,Barcode
+	for (var i=1; i < fileLines.length; i++) { //Exclude header line 0
+		if (fileLines[i] == '') return; //Account for empty trailing line
+		var barcodeData = fileLines[i].split(',');
+		var newBarcode = {supplierID:barcodeData[0], SKU:barcodeData[1], Barcode:barcodeData[2]};
+		barcodes.push(newBarcode);
+	}
+}
+
+//import given lines into catalogs objects
+//fileLines - Array of lines imported for file (string array)
+function importCatalogs(fileLines) {
+	//SKU,Description
+	for (var i=1; i < fileLines.length; i++) { //Exclude header line 0
+		if (fileLines[i] == '') return; //Account for empty trailing line
+		var catalogData = fileLines[i].split(',');
+		var newCatalog = {SKU:catalogData[0], Description:catalogData[1]};
+		catalogs.push(newCatalog);
+	}
+}
+
+//import given lines into suppliers objects
+//fileLines - Array of lines imported for file (string array)
+function importSuppliers(fileLines) {
+	//ID,Name
+	for (var i=1; i < fileLines.length; i++) { //Exclude header line 0
+		if (fileLines[i] == '') return; //Account for empty trailing line
+		var supplierData = fileLines[i].split(',');
+		var newSupplier = {ID:supplierData[0], Name:supplierData[1]};
+		suppliers.push(newSupplier);
+	}
 }
